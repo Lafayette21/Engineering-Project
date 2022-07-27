ec1d6b3e3b37e5cba4f1527b6d698c47fd56ec75
Lafayette21 <marcin.urbanowicz2345@gmail.com>
2022-07-27 21:34:31 +0200

    VisualisationGeneratorWindowController expanded but not working with fxml

--- Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/visualisationgeneratorwindow/VisualisationGeneratorWindowController.java
+++ Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/visualisationgeneratorwindow/VisualisationGeneratorWindowController.java
@@ -1,13 +1,33 @@
 package com.example.project.visualisationgeneratorwindow;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Slider;
+import javafx.scene.control.TextField;
 
-public class VisualisationGeneratorWindowController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class VisualisationGeneratorWindowController implements Initializable {
     @FXML
     private Slider actorsSlider;
 
-    public void cos(){
+    @FXML
+    private TextField actorsTextField;
 
+    int actorsAmount;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        actorsSlider.valueProperty().addListener(new ChangeListener<Number>() {
+            @Override
+            public void changed(ObservableValue<? extends Number> observableValue, Number oldNumber, Number newNumber) {
+                actorsAmount = (int) newNumber;
+                actorsTextField.setText(Integer.toString(actorsAmount));
+            }
+        });
     }
+
 }
