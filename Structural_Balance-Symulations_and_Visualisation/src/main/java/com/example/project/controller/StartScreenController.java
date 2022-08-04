007ddfaf944e4367ff9c550dc9c61cbcf7671462
Lafayette21 <marcin.urbanowicz2345@gmail.com>
2022-08-04 19:16:26 +0200

    Added new fxml screens placed under VisualisationGenerator

--- Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/controller/StartScreenController.java
+++ Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/controller/StartScreenController.java
@@ -1,10 +1,22 @@
 package com.example.project.controller;
 
+import com.example.project.Resource;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class StartScreenController implements ControlledScreen {
 
     private ScreenController controller;
+
+    @FXML
+    private Button startButton;
     @Override
     public void setScreenParent(ScreenController screenParent) {
         controller = screenParent;
+    }
+
+    public void changeScreenToVisualisationGenerator(ActionEvent event){
+        controller.setScreen(Resource.VisualisationGenerator);
     }
 }
