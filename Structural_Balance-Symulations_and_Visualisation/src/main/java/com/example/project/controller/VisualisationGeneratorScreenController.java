dafcc4f41764c6c63e93a4d6f7476681dbf1c152
Lafayette21 <marcin.urbanowicz2345@gmail.com>
2022-08-04 19:53:50 +0200

    ParametersScreen and VisualisationScreen controllers implemented with fxml files

--- Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/controller/VisualisationGeneratorScreenController.java
+++ Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/controller/VisualisationGeneratorScreenController.java
@@ -1,11 +1,20 @@
 package com.example.project.controller;
 
+import javafx.fxml.FXML;
+
 public class VisualisationGeneratorScreenController implements ControlledScreen {
 
     ScreenController controller;
 
+    @FXML private ParametersScreenController parametersScreenController;
+    @FXML private VisualisationScreenController visualisationScreenController;
+
     @Override
     public void setScreenParent(ScreenController screenParent) {
         controller = screenParent;
+    }
+
+    @FXML private void initialize(){
+
     }
 }
