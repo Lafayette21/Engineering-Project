0dc10aa9d2a8b69406f36bf9fb4fbd7741623130
Lafayette21 <marcin.urbanowicz2345@gmail.com>
2022-08-06 18:14:50 +0200

    Fixed sizes for window

--- Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/controller/parameters/ParametersScreenController.java
+++ Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/controller/parameters/ParametersScreenController.java
@@ -54,8 +54,6 @@ public class ParametersScreenController implements Initializable {
             }
             if (newValue.equals("Symulacja")) {
                 contentScreensHandler.setContentScreen(Resource.SimulationParameters);
-            } else {
-                throw new IllegalStateException("Resource not found");
             }
         }
     }
