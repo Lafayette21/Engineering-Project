94f44cfb457f91be27a8c7bc2ebc21c1f11d752a
Lafayette21 <marcin.urbanowicz2345@gmail.com>
2022-08-21 14:58:43 +0200

    Added new elements on simulation and actors screens

--- Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/controller/parameters/ActorsParametersScreenController.java
+++ Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/controller/parameters/ActorsParametersScreenController.java
@@ -1,7 +1,15 @@
 package com.example.project.controller.parameters;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Slider;
+
 public class ActorsParametersScreenController implements ParameterControlledScreen {
     private ParametersScreenController screenParent;
+
+    @FXML
+    private Slider actorsNumberSlider;
+
+
 
     @Override
     public void setScreenParent(ParametersScreenController screenParent) {
--- Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/controller/parameters/SimulationParametersScreenController.java
+++ Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/controller/parameters/SimulationParametersScreenController.java
@@ -1,7 +1,15 @@
 package com.example.project.controller.parameters;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
 public class SimulationParametersScreenController implements ParameterControlledScreen {
     private ParametersScreenController screenParent;
+
+    @FXML
+    private TextField stepNumberTextField;
+    @FXML
+    private TextField annealingTextField;
 
     @Override
     public void setScreenParent(ParametersScreenController screenParent) {
