30b17358eb3f97200b03a17d17088cd5b2440cad
Lafayette21 <marcin.urbanowicz2345@gmail.com>
2022-10-15 15:52:09 +0200

    Created class DatabaseHandler and a test for it

---
+++ Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/util/DatabaseHandler.java
@@ -0,0 +1,19 @@
+package com.example.project.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseHandler {
+    private static final String URL = "jdbc:h2:mem:test";
+
+    private final Connection connection;
+
+    public DatabaseHandler() {
+        try {
+            this.connection = DriverManager.getConnection(URL);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ Structural_Balance-Symulations_and_Visualisation/src/test/java/com/example/project/util/DatabaseHandlerTest.java
@@ -0,0 +1,12 @@
+package com.example.project.util;
+
+import org.junit.Test;
+
+public class DatabaseHandlerTest {
+
+    @Test
+    public void shouldEstablishConnection(){
+        new DatabaseHandler();
+    }
+
+}
