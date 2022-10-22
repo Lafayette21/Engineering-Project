24d1709a46b379f9633291bb0762e31925ca04ba
Lafayette21 <marcin.urbanowicz2345@gmail.com>
2022-10-22 19:09:02 +0200

    Changed RepositoryManager to be Singleton class

--- Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/database/repository/RepositoryManager.java
+++ Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/database/repository/RepositoryManager.java
@@ -4,9 +4,18 @@ import com.example.project.Resource;
 
 import java.util.HashMap;
 import java.util.Map;
-
-public class RepositoryManager {
+public final class RepositoryManager {
+    private static RepositoryManager repositoryManagerInstance;
     private final Map<Resource, ParameterRepository> parameterRepositoryMap = new HashMap<>();
+
+    private RepositoryManager() {}
+
+    public static RepositoryManager getInstance(){
+        if (repositoryManagerInstance == null){
+            repositoryManagerInstance = new RepositoryManager();
+        }
+        return repositoryManagerInstance;
+    }
 
     public void registerParameterRepository(Resource resource, ParameterRepository parameterRepository){
         parameterRepositoryMap.put(resource,parameterRepository);
