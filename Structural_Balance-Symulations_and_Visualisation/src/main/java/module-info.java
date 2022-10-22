30211e9a16ccffd712a2c99169e27740634b257f
Lafayette21 <marcin.urbanowicz2345@gmail.com>
2022-10-22 16:05:26 +0200

    Add dependencies needed for implementation of hibernate

--- Structural_Balance-Symulations_and_Visualisation/src/main/java/module-info.java
+++ Structural_Balance-Symulations_and_Visualisation/src/main/java/module-info.java
@@ -9,6 +9,7 @@ module com.example.structural_balancesymulations_and_visualisation {
     requires javafx.swing;
     requires java.sql;
     requires jakarta.persistence;
+    requires org.hibernate.orm.core;
 
     opens com.example.project to javafx.fxml;
     exports com.example.project;
@@ -16,4 +17,6 @@ module com.example.structural_balancesymulations_and_visualisation {
     opens com.example.project.controller to javafx.fxml;
     exports com.example.project.controller.parameters;
     opens com.example.project.controller.parameters to javafx.fxml;
+    exports com.example.project.database.model;
+    opens com.example.project.database.model;
 }
