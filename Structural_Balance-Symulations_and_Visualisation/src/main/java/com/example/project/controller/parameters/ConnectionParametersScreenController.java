47826fcf3d5d29d30b1eabf5c768535837a03ccc
Lafayette21 <marcin.urbanowicz2345@gmail.com>
2022-10-22 20:34:48 +0200

    Wrap usage of ConnectionParameters to ConnectionParametersScreenController and implement ChangeListener classes for Spinners

--- Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/controller/parameters/ConnectionParametersScreenController.java
+++ Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/controller/parameters/ConnectionParametersScreenController.java
@@ -1,8 +1,12 @@
 package com.example.project.controller.parameters;
 
 import com.example.project.Resource;
+import com.example.project.database.model.ConnectionParameters;
+import com.example.project.database.repository.ConnectionParametersRepository;
 import com.example.project.database.repository.RepositoryManager;
 import com.example.project.parametervalues.ConnectionsParametersValues;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -19,9 +23,9 @@ public class ConnectionParametersScreenController implements ParameterControlled
     private static final int STEP_SPINNER_VALUE = 5;
 
     @FXML
-    private Spinner<Integer> connectionPercentSpinner;
+    private Spinner<Integer> connectionExistenceSpinner;
     @FXML
-    private Spinner<Integer> positiveToNegativeRatioSpinner;
+    private Spinner<Integer> positiveRatioSpinner;
     @FXML
     private Button updateButton;
 
@@ -35,7 +39,7 @@ public class ConnectionParametersScreenController implements ParameterControlled
     public void updateParametersValues() {
         ParametersValueHandler valueHandler = screenParent.getParametersValueHandler();
         ConnectionsParametersValues connectionsParametersValues =
-                new ConnectionsParametersValues(connectionPercentSpinner.getValue(), positiveToNegativeRatioSpinner.getValue());
+                new ConnectionsParametersValues(connectionExistenceSpinner.getValue(), positiveRatioSpinner.getValue());
         valueHandler.updateValues(Resource.ConnectionParameters, connectionsParametersValues);
 
         SuccesAlertFactory.createAndShow();
@@ -43,8 +47,19 @@ public class ConnectionParametersScreenController implements ParameterControlled
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        setSpinnerValueFactory(connectionPercentSpinner);
-        setSpinnerValueFactory(positiveToNegativeRatioSpinner);
+        RepositoryManager repositoryManager = RepositoryManager.getInstance();
+        ConnectionParametersRepository repository =
+                (ConnectionParametersRepository) repositoryManager.getParameterRepositoryByResource(Resource.ConnectionParameters);
+
+        setSpinnerValueFactory(connectionExistenceSpinner);
+        setSpinnerValueFactory(positiveRatioSpinner);
+
+        ConnectionParameters connectionParameters =
+                new ConnectionParameters(connectionExistenceSpinner.getValue(), positiveRatioSpinner.getValue());
+        repository.registerConnectionParameters(connectionParameters);
+
+        connectionExistenceSpinner.valueProperty().addListener(new ConnectionExistencePercentageChangeListener(repository));
+        positiveRatioSpinner.valueProperty().addListener(new PositiveRatioChangeListener(repository));
     }
 
     private void setSpinnerValueFactory(Spinner<Integer> spinner) {
@@ -52,5 +67,31 @@ public class ConnectionParametersScreenController implements ParameterControlled
                 .IntegerSpinnerValueFactory(MIN_SPINNER_VALUE, MAX_SPINNER_VALUE, INITIAL_SPINNER_VALUE, STEP_SPINNER_VALUE);
 
         spinner.setValueFactory(valueFactory);
+    }
+
+    private class ConnectionExistencePercentageChangeListener implements ChangeListener<Integer> {
+        ConnectionParametersRepository repository;
+
+        public ConnectionExistencePercentageChangeListener(ConnectionParametersRepository repository) {
+            this.repository = repository;
+        }
+
+        @Override
+        public void changed(ObservableValue<? extends Integer> observableValue, Integer oldValue, Integer newValue) {
+            repository.updateConnectionExistencePercentage(newValue);
+        }
+    }
+
+    private class PositiveRatioChangeListener implements ChangeListener<Integer> {
+        ConnectionParametersRepository repository;
+
+        public PositiveRatioChangeListener(ConnectionParametersRepository repository) {
+            this.repository = repository;
+        }
+
+        @Override
+        public void changed(ObservableValue<? extends Integer> observableValue, Integer oldValue, Integer newValue) {
+            repository.updatePositiveConnectionsPercentage(newValue);
+        }
     }
 }
