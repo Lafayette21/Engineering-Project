8b9fe7ceff3f9fc0db1c1a27f947712322d3c03d
Lafayette21 <marcin.urbanowicz2345@gmail.com>
2022-11-03 20:17:15 +0100

    Add needed button funcitons in ControlPanelController and NetTabController

--- Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/controller/simulationflow/NetTabController.java
+++ Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/controller/simulationflow/NetTabController.java
@@ -1,6 +1,10 @@
 package com.example.project.controller.simulationflow;
 
 import com.example.project.database.model.SimulationParameters;
+import com.example.project.exception.SimulationBalanceAchievedException;
+import com.example.project.simulation.SimulationFlow;
+import com.example.project.simulation.SimulationRequiredValuesDTO;
+import com.example.project.util.SimulationBalanceAlert;
 import com.example.project.visualisation.model.Actor;
 import com.example.project.visualisation.model.Relation;
 import com.example.project.visualisation.screen.CanvasDrawer;
@@ -16,7 +20,39 @@ public class NetTabController {
     @FXML
     private AnchorPane visualisationPanel;
 
-    public void prepareInitialVisualisation(List<Actor> actorList, List<Relation> relationList, SimulationParameters parameters) {
+    private SimulationFlow simulationFlow;
+
+    public void prepareInitialVisualisation(SimulationRequiredValuesDTO requiredValues) {
+        List<Actor> actorList = requiredValues.actorList();
+        List<Relation> relationList = requiredValues.relationList();
+        SimulationParameters parameters = requiredValues.simulationParameters();
+        simulationFlow = new SimulationFlow(actorList, relationList, parameters);
         CanvasDrawer.draw(visualisationPanel, actorList, relationList);
+    }
+
+    public void nextSimulationStep() {
+        updateCurrentStepNumber();
+        try {
+            simulationFlow.nextStep(visualisationPanel);
+        } catch (SimulationBalanceAchievedException e) {
+            new SimulationBalanceAlert().showAndWait();
+        }
+    }
+
+    public void skipToEnd() {
+        try {
+            simulationFlow.skipToEnd(visualisationPanel);
+        } catch (SimulationBalanceAchievedException e) {
+            new SimulationBalanceAlert().showAndWait();
+        }
+    }
+
+    public void previousSimulationStep() {
+        updateCurrentStepNumber();
+        simulationFlow.previousStep(visualisationPanel);
+    }
+
+    private void updateCurrentStepNumber() {
+        statePanelController.updateStepNumber(simulationFlow.getCurrentStepNumber());
     }
 }
