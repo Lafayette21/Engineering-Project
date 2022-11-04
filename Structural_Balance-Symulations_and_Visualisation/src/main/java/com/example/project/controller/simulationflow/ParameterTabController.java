921c49f4276d5f181829f94fbf455460504d13f2
Lafayette21 <marcin.urbanowicz2345@gmail.com>
2022-11-04 18:52:26 +0100

    Add labels and spinners to ParameterTab

--- Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/controller/simulationflow/ParameterTabController.java
+++ Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/controller/simulationflow/ParameterTabController.java
@@ -1,8 +1,61 @@
 package com.example.project.controller.simulationflow;
 
+import com.example.project.database.repository.RepositoryManager;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.SpinnerValueFactory;
 
-public class ParameterTabController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ParameterTabController implements Initializable {
     @FXML
     private StatePanelController statePanelController;
+
+    @FXML
+    private Spinner<Integer> stepNumberSpinner;
+    @FXML
+    private Spinner<Double> temperatureSpinner;
+    @FXML
+    private Spinner<Integer> timeSpinner;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        RepositoryManager repositoryManager = RepositoryManager.getInstance();
+
+        setStepNumberSpinnerValueFactory();
+        setTimeSpinnerValueFactory();
+        setTemperatureSpinnerValueFactory();
+    }
+
+    private void setStepNumberSpinnerValueFactory() {
+        int minSpinnerValue = 50;
+        int maxSpinnerValue = 300;
+        int initialSpinnerValue = 50;
+        int stepSpinnerValue = 25;
+        SpinnerValueFactory<Integer> valueFactory = new SpinnerValueFactory
+                .IntegerSpinnerValueFactory(minSpinnerValue, maxSpinnerValue, initialSpinnerValue, stepSpinnerValue);
+        stepNumberSpinner.setValueFactory(valueFactory);
+    }
+
+    private void setTimeSpinnerValueFactory() {
+        int minSpinnerValue = 0;
+        int maxSpinnerValue = 60;
+        int initialSpinnerValue = 0;
+        int stepSpinnerValue = 1;
+        SpinnerValueFactory<Integer> valueFactory = new SpinnerValueFactory
+                .IntegerSpinnerValueFactory(minSpinnerValue, maxSpinnerValue, initialSpinnerValue, stepSpinnerValue);
+        timeSpinner.setValueFactory(valueFactory);
+    }
+
+    private void setTemperatureSpinnerValueFactory() {
+        double minSpinnerValue = 0;
+        double maxSpinnerValue = 100;
+        double initialSpinnerValue = 0;
+        double stepSpinnerValue = 0.3;
+        SpinnerValueFactory<Double> valueFactory = new SpinnerValueFactory
+                .DoubleSpinnerValueFactory(minSpinnerValue, maxSpinnerValue, initialSpinnerValue, stepSpinnerValue);
+        temperatureSpinner.setValueFactory(valueFactory);
+    }
 }
