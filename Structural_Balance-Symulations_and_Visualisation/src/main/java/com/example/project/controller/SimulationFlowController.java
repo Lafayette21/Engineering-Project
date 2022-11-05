d8021463301e722883c9a9b418c6aa5930a2a553
Lafayette21 <marcin.urbanowicz2345@gmail.com>
2022-11-05 15:05:56 +0100

    Wrap usage of interface TabController to SimulationFlowController

--- Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/controller/SimulationFlowController.java
+++ Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/controller/SimulationFlowController.java
@@ -1,19 +1,25 @@
 package com.example.project.controller;
 
-import com.example.project.controller.simulationflow.ChartTabController;
-import com.example.project.controller.simulationflow.ControlPanelController;
-import com.example.project.controller.simulationflow.NetTabController;
-import com.example.project.controller.simulationflow.ParameterTabController;
+import com.example.project.RepositoryName;
+import com.example.project.controller.simulationflow.*;
+import com.example.project.database.model.NewSimulationParameters;
+import com.example.project.database.repository.NewSimulationParametersRepository;
+import com.example.project.database.repository.RepositoryManager;
+import com.example.project.simulation.SimulationFlow;
 import com.example.project.simulation.SimulationRequiredValuesDTO;
+import com.example.project.visualisation.model.Actor;
+import com.example.project.visualisation.model.Relation;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
 
 public class SimulationFlowController implements ControlledScreen, Initializable {
     private final MainApplicationScreenController screenParent = MainApplicationScreenController.getInstance();
-
+    private final NewSimulationParametersRepository repository = (NewSimulationParametersRepository) RepositoryManager
+            .getInstance().getParameterRepositoryByName(RepositoryName.NEW_SIMULATION_PARAMETERS);
     @FXML
     private NetTabController netTabController;
     @FXML
@@ -23,8 +29,11 @@ public class SimulationFlowController implements ControlledScreen, Initializable
     @FXML
     private ControlPanelController controlPanelController;
 
+    private SimulationFlow simulationFlow;
+
+
     public void nextStep() {
-        netTabController.nextSimulationStep();
+        getAllControllers().forEach(TabController::nextSimulationStep);
     }
 
     public void previousStep() {
@@ -43,6 +52,14 @@ public class SimulationFlowController implements ControlledScreen, Initializable
 
     private void prepareInitialSimulationVisualisation() {
         SimulationRequiredValuesDTO requiredValuesDTO = (SimulationRequiredValuesDTO) screenParent.getUserData();
-        netTabController.prepareInitialVisualisation(requiredValuesDTO);
+        List<Actor> actorList = requiredValuesDTO.actorList();
+        List<Relation> relationList = requiredValuesDTO.relationList();
+        NewSimulationParameters parameters = repository.getSimulationParameters();
+        simulationFlow = new SimulationFlow(actorList, relationList, parameters);
+        netTabController.prepareInitialVisualisation(actorList, relationList, simulationFlow);
+    }
+
+    private List<TabController> getAllControllers() {
+        return List.of(this.netTabController, this.chartTabController, this.parameterTabController);
     }
 }
