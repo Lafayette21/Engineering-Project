b3aea52ba2d465725b6d618b2874b2a790d139fe
Lafayette21 <marcin.urbanowicz2345@gmail.com>
2022-11-05 17:46:33 +0100

    Make fixes to AverageCalculator

--- Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/simulation/AverageCalculator.java
+++ Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/simulation/AverageCalculator.java
@@ -1,15 +1,15 @@
 package com.example.project.simulation;
 
 public class AverageCalculator {
-    public static double calculate(ConnectionMatrix connectionMatrix, RelationMatrix relationMatrix){
+    public static double calculate(ConnectionMatrix connectionMatrix, RelationMatrix relationMatrix) {
         double sum = 0;
         double L = 0;
-        for (int i=0;i< connectionMatrix.getNumberOfActors();i++){
-            for (int j=0;j<i;j++){
-                sum+= connectionMatrix.get(i,j)* relationMatrix.get(i,j);
-                L+= connectionMatrix.get(i,j);
+        for (int i = 0; i < connectionMatrix.getNumberOfActors(); i++) {
+            for (int j = 0; j < i; j++) {
+                sum += connectionMatrix.get(i + 1, j + 1) * relationMatrix.get(i + 1, j + 1);
+                L += connectionMatrix.get(i + 1, j + 1);
             }
         }
-        return sum/L;
+        return sum / L;
     }
 }
