acbb0f8f6283fb24f85db49ecfac79ae98e95734
Lafayette21 <marcin.urbanowicz2345@gmail.com>
2022-11-12 00:30:22 +0100

    Create class TriadDrawer

---
+++ Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/visualisation/screen/TriadDrawer.java
@@ -0,0 +1,35 @@
+package com.example.project.visualisation.screen;
+
+import com.example.project.exception.InstantiationNotAllowedException;
+import com.example.project.visualisation.model.Actor;
+import com.example.project.visualisation.model.Triad;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
+
+import java.util.List;
+
+public class TriadDrawer {
+    private TriadDrawer() {
+        throw new InstantiationNotAllowedException();
+    }
+
+    public static void drawTriadTriangle(AnchorPane visualisationPane, List<Triad> triadList){
+        for (Triad triad : triadList){
+            Color typeLevelColor = triad.getTypeLevel().getColor();
+            Polygon polygon = createPolygon(triad, typeLevelColor);
+            visualisationPane.getChildren().add(polygon);
+        }
+    }
+
+    private static Polygon createPolygon(Triad triad, Color typeLevelColor) {
+        Polygon polygon = new Polygon();
+        triad.getTriadActors().stream()
+                .map(Actor::getPosition)
+                .forEach(position -> polygon.getPoints()
+                            .addAll(position.getX(), position.getY())
+                );
+        polygon.setFill(typeLevelColor);
+        return polygon;
+    }
+}
