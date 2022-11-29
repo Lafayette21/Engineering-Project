7044b52d51eb0d4cdba9e0e62fe71a8e16019cae
Lafayette21 <marcin.urbanowicz2345@gmail.com>
2022-11-29 18:23:00 +0100

    Change size of window

--- Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/controller/MainApplicationScreenController.java
+++ Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/controller/MainApplicationScreenController.java
@@ -9,8 +9,8 @@ import javafx.scene.layout.AnchorPane;
 import java.util.HashMap;
 
 public class MainApplicationScreenController extends AnchorPane {
-    private static final double HEIGHT = 450.0;
-    private static final double WIDTH = 650.0;
+    private static final double HEIGHT = 800.0;
+    private static final double WIDTH = 1200.0;
 
     private final HashMap<Resource, Node> screens = new HashMap<>();
 
