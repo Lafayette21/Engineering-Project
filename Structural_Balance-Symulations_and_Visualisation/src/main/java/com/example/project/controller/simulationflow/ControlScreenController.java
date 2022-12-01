5e9e44445272bf85a038a4df92dbb7d3776f6e91
Lafayette21 <marcin.urbanowicz2345@gmail.com>
2022-12-01 18:34:38 +0100

    Create buttons in ControlScreen

--- Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/controller/simulationflow/ControlScreenController.java
+++ Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/controller/simulationflow/ControlScreenController.java
@@ -1,18 +1,43 @@
 package com.example.project.controller.simulationflow;
 
 import com.example.project.controller.SimulationFlowController2;
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
 public class ControlScreenController {
+    private static final String BUTTON_START_TEXT = "Rozpocznij";
+    private static final String BUTTON_PAUSE_TEXT = "Zatrzymaj";
+
+    @FXML
+    private Button previousStepButton;
+    @FXML
+    private Button nextStepButton;
+    @FXML
+    private Button runButton;
+
     private SimulationFlowController2 parentController;
 
     public void injectScreenController(SimulationFlowController2 simulationFlowController2){
         this.parentController = simulationFlowController2;
     }
 
-    public Button nextStepButton;
-
     public void nextStep(){
         parentController.nextStep();
+    }
+
+    public void previousStep(){
+        parentController.previousStep();
+    }
+
+    public void start(){
+        parentController.start();
+        runButton.setText(BUTTON_PAUSE_TEXT);
+        runButton.setOnAction(event -> pause());
+    }
+
+    public void pause(){
+        parentController.pause();
+        runButton.setText(BUTTON_START_TEXT);
+        runButton.setOnAction(event -> start());
     }
 }
