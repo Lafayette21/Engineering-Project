5c4cf6ee8134988203a0bb387643f289886770e7
Lafayette21 <marcin.urbanowicz2345@gmail.com>
2022-12-01 17:05:53 +0100

    Create FXML dependencies in SimulationFlowController2 and Add id from stateController to SimulationFlowScreen2

--- Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/controller/SimulationFlowController2.java
+++ Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/controller/SimulationFlowController2.java
@@ -1,4 +1,24 @@
 package com.example.project.controller;
 
-public class SimulationFlowController2 implements ControlledScreen {
+import com.example.project.controller.simulationflow.ChartScreenController;
+import com.example.project.controller.simulationflow.NetScreenController;
+import com.example.project.controller.simulationflow.StatePanelController;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SimulationFlowController2 implements ControlledScreen, Initializable {
+    @FXML
+    private NetScreenController netScreenController;
+    @FXML
+    private ChartScreenController chartScreenController;
+    @FXML
+    private StatePanelController statePanelController;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        System.out.println();
+    }
 }
