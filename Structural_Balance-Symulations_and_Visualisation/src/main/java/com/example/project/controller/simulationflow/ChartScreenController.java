bc9714464a67aada7e1c3598244a03b0f21cfd44
Lafayette21 <marcin.urbanowicz2345@gmail.com>
2022-12-01 18:15:39 +0100

    Add LineChart to ChartScreen

--- Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/controller/simulationflow/ChartScreenController.java
+++ Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/controller/simulationflow/ChartScreenController.java
@@ -1,4 +1,9 @@
 package com.example.project.controller.simulationflow;
 
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+
 public class ChartScreenController {
+    @FXML
+    private LineChart<String, Double> chart;
 }
