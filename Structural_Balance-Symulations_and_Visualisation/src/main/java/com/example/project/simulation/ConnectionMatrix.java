ac0493f8321318a3030df982389da7844ea095bf
Lafayette21 <marcin.urbanowicz2345@gmail.com>
2022-12-03 14:49:02 +0100

    Make minor clearings in ConnectionMatrix and RelationMatrix and apply usage of RelationMatrix to RelationMatrixToRelationListConverter

--- Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/simulation/ConnectionMatrix.java
+++ Structural_Balance-Symulations_and_Visualisation/src/main/java/com/example/project/simulation/ConnectionMatrix.java
@@ -8,23 +8,19 @@ import java.util.List;
 public class ConnectionMatrix {
     private static final int RELATION_EXISTENCE_ANNOTATION = 1;
 
-    private final List<Relation> relationList;
     private final int numberOfActors;
     private int[][] matrix;
 
     public ConnectionMatrix(List<Relation> relationList, int numberOfActors) {
-        this.relationList = relationList;
         this.numberOfActors = numberOfActors;
-        createConnectionMatrix();
+        createConnectionMatrix(relationList);
     }
 
-    private void createConnectionMatrix() {
+    private void createConnectionMatrix(List<Relation> relationList) {
         matrix = new int[numberOfActors][numberOfActors];
-        for (Relation relation : relationList) {
-            if (existsRelation(relation)) {
-                setRelationInMatrix(relation);
-            }
-        }
+        relationList.stream()
+                .filter(this::existsRelation)
+                .forEach(this::setRelationInMatrix);
     }
 
     private boolean existsRelation(Relation relation) {
@@ -38,16 +34,16 @@ public class ConnectionMatrix {
         matrix[secondActorId - 1][firstActorId - 1] = RELATION_EXISTENCE_ANNOTATION;
     }
 
-    public int get(int firstActorId, int secondActorId) {
+    int get(int firstActorId, int secondActorId) {
         return matrix[firstActorId - 1][secondActorId - 1];
     }
 
-    public boolean existsRelation(int firstActorId, int secondActorId) {
+    boolean existsRelation(int firstActorId, int secondActorId) {
         return matrix[firstActorId - 1][secondActorId - 1] == RELATION_EXISTENCE_ANNOTATION ||
                 matrix[secondActorId - 1][firstActorId - 1] == RELATION_EXISTENCE_ANNOTATION;
     }
 
-    public int getNumberOfExistingRelations() {
+    int getNumberOfExistingRelations() {
         int sum = 0;
         for (int i = 0; i < numberOfActors; i++) {
             for (int j = 0; j < i; j++) {
@@ -61,7 +57,7 @@ public class ConnectionMatrix {
         return matrix;
     }
 
-    public int getNumberOfActors() {
+    int getNumberOfActors() {
         return numberOfActors;
     }
 }
